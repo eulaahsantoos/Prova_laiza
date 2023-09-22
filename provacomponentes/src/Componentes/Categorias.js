c1fb4087d159759ea0a39faac92482c6aed73c3f
eulaahsantoos <laizasantos616@gmail.com>
2023-09-22 17:49:44 +0000

    aula2209

---
+++ provacomponentes/src/Componentes/Categorias.js
@@ -0,0 +1,17 @@
+function Categorias(props){
+
+    return(
+      <div>
+        <p> {props.Categorias} Seis homens morreram em confrontos com policiais durante operação na região de Águas Claras, 
+        em Salvador, e em Feira de Santana (BA) na manhã desta sexta-feira (22). Houve 15 prisões. Segundo a polícia,
+         os mortos faziam parte de um grupo criminoso suspeito de matar mais de 30 pessoas.
+        "Esse grupo é responsável por mais de 30 homicídios", explicou a diretora do DHPP, delegada Andréa Ribeiro.
+         A Bahia vive, desde junho, uma onda de violência marcada por tiroteios. Operações policiais têm provocado mortes nas últimas semanas.
+         A última semana foi a mais letal deste mês. Isso porque 20 pessoas foram mortas desde o dia 15,
+          incluindo os 6 homens desta sexta. Ao todo, em setembro, 34 pessoas morreram durante operações policiais. </p>
+    
+      </div> 
+    )  
+}
+
+export default Categorias;
---
+++ provacomponentes/src/Componentes/noticias.js
@@ -0,0 +1,17 @@
+
+import './noticias.Css';
+
+function noticias() {
+  return (
+    <div className="Container">
+ <h1>Megaoperação' contra suspeitos de matar mais de 30 pessoas na Bahia deixa 6 mortos/</h1>
+
+ <p>Ação policial começou nas primeiras horas desta sexta-feira (22), em Águas Claras,
+     em Salvador. Suspeitos também têm envolvimento com tráfico de drogas.
+     Por João Souza, Monica Melo, Fernanda Martinez, g1 BA e TV Bahia
+    22/09/2023 06h22 </p>
+    </div>
+  );
+}
+
+export default noticias;
